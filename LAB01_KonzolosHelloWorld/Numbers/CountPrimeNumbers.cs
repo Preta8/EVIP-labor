be00f023edd680b15b1bf15b09c593d81aa3e2f9
Petra <kescash@gmail.com>
2019-09-12 20:21:31 +0200

    4. feladat kész

---
+++ LAB01_KonzolosHelloWorld/Numbers/CountPrimeNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Numbers
+{
+    publiv class CountPrimeNumbers : SolutionProviderBase
+    {
+        private int upperLimit;
+
+        public CountPrimeNumbers(int upperLimit)
+        {
+            this.upperLimit = upperLimit;
+        }
+        public override int CalculateSolution()
+        {
+            int cnt = 0;
+            for (int i = 2; i < upperLimit; i++)
+            {
+                if (IsPrime(i))
+                {
+                    cnt += i;
+                }
+            }
+            return cnt;
+        }
+
+        private bool IsPrime(int n)
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (n %i == 0)
+                
+                    return false;
+                
+            }
+            return true;
+
+           
+        }
+    }
+}
--- LAB01_KonzolosHelloWorld/Numbers/Program.cs
+++ LAB01_KonzolosHelloWorld/Numbers/Program.cs
@@ -13,10 +13,17 @@ namespace Numbers
             solution = new SumOfNumbers(10);
             Console.WriteLine("SumOfNumbers: " + solution.GetSolutionText());
 
+            solution = new CountPrimeNumbers(100);
+            Console.WriteLine("SumOfPrimeNumbers: " + solution.GetSolutionText());
+
             solution = new SolutionDecorator(new SumOfNumbers(10));
             Console.WriteLine("Decorated SumOfNumbers: " + solution.GetSolutionText());
 
+            solution = new SolutionDecorator(new CountPrimeNumbers(100));
+            Console.WriteLine("Decorated SumOfPrimeNumbers: " + solution.GetSolutionText());
+
             Console.ReadKey();
+            
         }
     }
 }
